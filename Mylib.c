574f2c37ee006156895eb1bb9c956c8f2d09111b
fabio <fabioaraujo.js@hotmail.com>
2019-08-12 15:11:09 -0300

    marcelo é um idiota

--- Mylib.c
+++ Mylib.c
@@ -6,7 +6,11 @@ void imp_menu(void){
     system("Clear");
     printf("-------- CARS FLOW---------\n");
     printf("1- Cadatrar \n ");
-    printf("2- Histórico \n");
+    printf("2- cadastro temporário \n");
+    printf("3- Histórico \n");
+    printf("4- Sobre \n");
+    printf("5- Sair \n");                                                                                                        cc \n");
+
     
     
 
